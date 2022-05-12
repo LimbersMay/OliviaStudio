55eed4cad701181e5223d3a217310529ed9b060d
limbers may <limbermay1@gmail.com>
2022-05-12 10:58:49 -0500

    se agregó el botón de cerrar sesión en perfil estudiante

--- app/src/main/java/com/olivia/studio/Categoria_Docente.java
+++ app/src/main/java/com/olivia/studio/Categoria_Docente.java
@@ -1,8 +1,9 @@
 package com.olivia.studio;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Categoria_Docente extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class Categoria_Docente extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categoria_docente);
+    }
+
+    public void volver(View view){
+        Intent volverPerfil = new Intent(this, PerfilDocenteActivity.class);
+        startActivity(volverPerfil);
+
+        finish();
     }
 }
