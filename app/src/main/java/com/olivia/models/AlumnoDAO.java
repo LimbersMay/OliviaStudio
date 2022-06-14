78353a943d9a310425cdd9d99f69a663765d4da7
limbersMay <limbermay1@gmail.com>
2022-06-14 17:04:19 -0500

    se agregó el método para registrar a un alumno

--- app/src/main/java/com/olivia/models/AlumnoDAO.java
+++ app/src/main/java/com/olivia/models/AlumnoDAO.java
@@ -1,4 +1,36 @@
 package com.olivia.models;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.olivia.domain.Alumno;
+import com.olivia.helpers.AdminSQLiteOpenHelper;
+
 public class AlumnoDAO {
+
+    private Context context;
+
+    public AlumnoDAO(Context context){
+        this.context = context;
+    }
+
+    public void registrarAlumno(Alumno alumno){
+
+        // Nos conectamos a la base de datos
+        AdminSQLiteOpenHelper adminDB = new AdminSQLiteOpenHelper(context, "administracion", null, 1);
+        SQLiteDatabase BaseDeDatos = adminDB.getReadableDatabase();
+
+        // Creamos el contenido para la base de datos
+        ContentValues registro = new ContentValues();
+
+        registro.put("nombre", alumno.getNombre());
+        registro.put("correo", alumno.getCorreo());
+        registro.put("contrasenia", alumno.getContrasenia());
+        registro.put("pais", alumno.getPais());
+        registro.put("codigoPostal", alumno.getCodigoPostal());
+
+        BaseDeDatos.insert("Alumno", null, registro);
+        BaseDeDatos.close();
+    }
 }
